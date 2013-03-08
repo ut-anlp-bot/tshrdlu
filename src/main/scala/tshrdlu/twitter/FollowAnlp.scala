c23322599326952905fb5e7f1f3efd7c941af44d
Nazneen Rajani <emailnazneen@gmail.com>
2013-03-08 01:32:09 -0600

    Added Follow Anlp code

---
+++ src/main/scala/tshrdlu/twitter/FollowAnlp.scala
@@ -0,0 +1,18 @@
+package tshrdlu.twitter
+
+import twitter4j._
+
+object FollowAnlp extends TwitterInstance with StreamInstance{
+
+  def main(args: Array[String]) {
+    
+       val followerIds = twitter.getFollowersIDs("appliednlp",-1).getIDs
+    val anlp = followerIds.map{id => twitter.showUser(id)}.toSet
+    val names = anlp.map{x => x.getScreenName}.toSet
+   val anlp_names = names.filter{x => (x.endsWith("_anlp") && x!="nrajani_anlp")}
+   //anlp_names-"nrajani_anlp"
+   println(anlp_names)
+   anlp_names.foreach(twitter.createFriendship)
+  }
+
+}
