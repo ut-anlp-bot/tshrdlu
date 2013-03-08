568e30cc819fe6c21289a350b3e13c3972dbc72e
Nazneen Rajani <emailnazneen@gmail.com>
2013-03-08 01:59:34 -0600

    Modified and cleaned Follow_Anlp code

--- src/main/scala/tshrdlu/twitter/FollowAnlp.scala
+++ src/main/scala/tshrdlu/twitter/FollowAnlp.scala
@@ -8,11 +8,8 @@ import twitter4j._
 object FollowAnlp extends TwitterInstance with StreamInstance{
 
   def main(args: Array[String]) {
-    val followerIds = twitter.getFollowersIDs("appliednlp",-1).getIDs
-    val anlp = followerIds.map{id => twitter.showUser(id)}.toSet
-    val names = anlp.map{x => x.getScreenName}.toSet
-    val anlp_names = names.filter{x => (x.endsWith("_anlp") && x!=twitter.getScreenName)}
-    println(anlp_names)
+    val followers = twitter.getFollowersIDs("appliednlp",-1).getIDs.map{id => twitter.showUser(id)}.toSet
+    val anlp_names = followers.map{x => x.getScreenName}.toSet.filter{x => (x.endsWith("_anlp") && x!=twitter.getScreenName)}
     anlp_names.foreach(twitter.createFriendship)
   }
 
