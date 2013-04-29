a957426842ec2c429795511a6acee4c321939e59
Nick Wilson <nick@njwilson.net>
2013-04-28 21:23:59 -0500

    Improve real/fake classifier. Add more data.

--- src/main/scala/tshrdlu/classify/RealFake.scala
+++ src/main/scala/tshrdlu/classify/RealFake.scala
@@ -3,6 +3,8 @@ package tshrdlu.classify
 import chalk.lang.eng.Twokenize
 import nak.data.{Featurizer, FeatureObservation}
 
+import tshrdlu.util.POSTagger
+
 
 /*
  * Basic featurizer for real tweet detection.
@@ -22,6 +24,33 @@ object BasicRealFakeFeaturizer extends Featurizer[String, String] {
  */
 object ExtendedRealFakeFeaturizer extends Featurizer[String, String] {
   def apply(content: String): Seq[FeatureObservation[String]] = {
-    BasicRealFakeFeaturizer(content)
+    val tokensAndTags = POSTagger(content).map { taggedToken =>
+      (taggedToken.token, taggedToken.tag)
+    }
+    val (tokens, posTags) = tokensAndTags.unzip
+
+    // Part-of-Speech N-Grams
+    val begin = List("<begin>")
+    val end = List("<end>")
+    val posUnigramFeatures = posTags.map { tag =>
+      FeatureObservation("pos_unigram=" + tag)
+    }
+    val posBigramFeatures = (begin ++ posTags ++ end).sliding(2).map {
+      case List(tag1, tag2) => FeatureObservation("pos_bigram=" + tag1 + tag2)
+    }
+    val posTrigramFeatures = (begin ++ begin ++ posTags ++ end ++ end).sliding(3).map {
+      case List(tag1, tag2, tag3) => FeatureObservation("pos_trigram=" + tag1 + tag2 + tag3)
+    }
+    val posNgramFeatures = posUnigramFeatures ++ posBigramFeatures ++ posTrigramFeatures
+
+    // Fraction of POS tags for each tag
+    val posFractions = posTags.groupBy(identity).mapValues(_.length).map {
+      case (tag, count) => FeatureObservation("pos_fraction_" + tag, count.toDouble / posTags.length)
+    }
+
+    val features = posNgramFeatures ++ posFractions
+    //features.foreach(println)
+    //println("")
+    features
   }
 }
